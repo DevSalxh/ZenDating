ca45ec7a8346dc300e4db45fc0c1c1d83070fdfc
DevSalxh <devsalxh@gmail.com>
2025-08-15 19:19:37 +0300

    Adding Authentication and Jwt Brearer for Autherization

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register([FromBody] RegisterDto registerDto)
+    {
+        if (await DoesUserExist(registerDto.Email)) return BadRequest("this email has been taken");
+
+        using var hmac = new HMACSHA256();
+        var user = new AppUser()
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            DisplayName = user.DisplayName,
+            Token = tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login([FromBody] LoginDto loginDto)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(u => u.Email.ToLower() == loginDto.Email.ToLower());
+        if (user == null) return Unauthorized("Invalid Email!");
+
+        using var hmac = new HMACSHA256(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password!");
+        }
+        return new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            DisplayName = user.DisplayName,
+            Token = tokenService.CreateToken(user)
+        };
+    }
+
+    private async Task<bool> DoesUserExist(string Email)
+    {
+        return await context.Users.AnyAsync(u => u.Email.ToLower() == Email.ToLower());
+    }
+}
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,40 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService(IConfiguration config) : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot reach the key!");
+        if (tokenKey.Length < 64) throw new Exception("your token length must be 64+ ");
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Email, user.Email),
+            new(ClaimTypes.NameIdentifier, user.Id)
+        };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+        var tokenDiscriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDiscriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
